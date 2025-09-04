ffe7ead3d1ebddb0dbd9a949ff0fe9459d7733aa
Chase Stapleton <cstaple10@gmail.com>
2025-09-04 13:28:32 -0500

    quiz 3

--- CountDigits/src/CountDigits.java
+++ CountDigits/src/CountDigits.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class CountDigits {
+    public static void main(String[] args) {
+        int count = 0;
+
+        for (int i = 0; i <= 100; i++)
+        {
+            String number = String.valueOf(i);
+
+            for (int j = 0; j < number.length(); j++)
+                if (number.charAt(j) == '8') count++;
+        }
+
+        System.out.println("# of 8 appearances from 0 to 100: " + count);
+    }
 }
---
+++ RunLengthEncode/src/RunLengthEncode.java
@@ -0,0 +1,22 @@
+public class RunLengthEncode {
+    public static String runLengthEncode(String str) {
+        StringBuilder encoded = new StringBuilder();
+        int count = 1;
+        for (int i = 1; i < str.length(); i++) {
+            if (i < str.length() && str.charAt(i) == str.charAt(i - 1)) {
+                count++;
+            }  else {
+                encoded.append(count);
+                encoded.append(str.charAt(i-1));
+                count = 1;
+            }
+        }
+        return encoded.toString();
+    }
+
+    public static void main(String[] args) {
+        String test = "AAAAABBBCCDAA";
+        System.out.println("Original: " + test);
+        System.out.println("Encoded: " + runLengthEncode(test));
+    }
+}
